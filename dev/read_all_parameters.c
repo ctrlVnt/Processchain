aef5511ba1107ec7011e8eac63e82a68ef50d78f
botu27 <b.turcan2705@gmail.com>
2021-12-16 12:55:33 -0800

    file di parsificazione aggiornato

--- dev/read_all_parameters.c
+++ dev/read_all_parameters.c
@@ -2,6 +2,37 @@
 #include <string.h>
 #include <stdlib.h>
 
+/*
+    Questo mini-programma esemplifica la parsificazione dei parametri utilizzati nelle simulazioni del progetto di SO
+    questo repo rappresenta proprio il progetto.
+    
+    L'idea di base: parsificare i parametri da un file TXT chiamato "parameters.txt",
+    la scelta dell'estensione del file e' arbitraria, il deve-team ha scelto come estensione .TXT, appunto.
+    
+    I parametri presenti nel file secondo il seguente pattern: NOME_VARIABILE=VALORE_ASSEGNATO.
+    Carattere '=' in questo caso e' chiamato delimitatore.
+
+    Breve panoramica dell'algoritmo:
+    - File viene aperto in lettura
+    - Viene parsificato per riga, ovvero sequenza il cui carattere finale e' '\n',
+    per questo motivo abbiamo utilizzato la funzione fgets (man fgets per approfondimenti) che accumula nel buffer i caratteri finche' non raggiunge CR('\n') oppure EOF
+    -Ogni blocco cosi' letto viene tokennizato -- funzione strtok che prende due paramteri: stringa da espezionare e il delimitatore, che non e' altro che una stringa
+    indicante quali byte di caratteri sono consedirati delimitatori, quindi restituisce il puntatore a stringa che contenuta tra caratteri-delimitatori, quando non ci sono piu' token utili, viene restituito NULL
+    (man strtok per approfondimenti)
+    -Considerando il pattern della stringa otteuta da fgets, possiamo desumenre che ad ogni lettura, successivamente vengono generati 2 token(in condizioni ottimali):
+    primo rappresenta il nome della variabile a cui vogliamo assegnare il valore che segue, la discriminazione delle variabile e' effettuata tramite la funzione strcmp
+    che ritorna 0 se le 2 stringhe passate come argomento sono lessicograficamente identiche. (man strcmp per approfondimenti)
+    -Token successivo che viene ricevuto non e' altro che il valore, che a sua volta viene castato con la funzione atoX, in base al tipom di valore, che puo' essere
+    un intero oppure un intero long -- attenzione atoX non gestiscono eventuali errori!
+    -La lettura viene effettuata fino al raggiungimento della fine del FILE(EOF).
+    -Quindi i parametri cosi' ottenuti vengono stampati a video.
+    
+    P.S.: La parsificazione cosi' defenita prevede un minimo di controllo sul nome della variabile, nel caso si verifica un errore di parsing, il file viene
+    chiuso e il programma termina.
+    P.S.S: Possibili miglioramenti: assumere che nessun parametro possa avere il valore 0(nel caso di atoX fallito, viene restituito lo 0), quindi
+    effetuare il controllo in ogni blocco dove appare la chiamata della funzione atoX e segnalare la presenza dell'eventuale errore.
+*/
+
 int SO_USERS_NUM;
 int SO_NODES_NUM;
 int SO_REWARD;
@@ -16,7 +47,6 @@ int SO_REGISTRY_SIZE;
 int SO_BUDGET_INIT;
 int SO_SIM_SEC;
 int SO_FRIENDS_NUM;
-
 
 int main()
 {
