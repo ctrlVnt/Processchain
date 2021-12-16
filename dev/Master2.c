3f43105de24a0ffe1a62117b2bcd1e8804839641
Hard.h <80196285+liveprog@users.noreply.github.com>
2021-12-16 16:31:26 +0100

    Aggiunta creazione fili nodo e users

--- dev/Master2.c
+++ dev/Master2.c
@@ -35,6 +35,52 @@ int main()
 	int SO_REGISTRY_SIZE, SO_BLOCK_SIZE;
 	SO_REGISTRY_SIZE = 10; //valore da inizializzare a runtime da file parameters.txt
 	SO_BLOCK_SIZE = 10;		//valore da inizializzare a runtime da file parameters.txt
+	
+	
+	int SO_NODES_NUM, SO_USERS_NUM;
+	SO_USERS_NUM = 3;
+	SO_NODES_NUM = 5;
+	int indice;
+  	unsigned int my_pid, my_ppid, value;
+
+	// creo n figli nodi
+
+  	for (indice=0; indice<SO_NODES_NUM; indice++) {
+
+    value = fork();
+
+    switch (value){
+    case -1:
+      printf("errore, ciao\n");
+      exit(EXIT_FAILURE);
+      
+    case 0:
+		printf("PID=%d, PPID=%d, i=%d, fork_value=%d\n", getpid(), getppid(), indice, value);
+		printf("ciao, sono NODO\n");
+		exit(EXIT_SUCCESS);
+    default:
+    printf("I'm a mother\n");
+    }
+  }
+	// creo n figli user
+  for (indice=0; indice<SO_USERS_NUM; indice++) {
+
+    value = fork();
+
+    switch (value)
+    {
+    case -1:
+      printf("errore, ciao\n");
+      exit(EXIT_FAILURE);
+      
+    case 0:
+      printf("PID=%d, PPID=%d, i=%d, fork_value=%d\n", getpid(), getppid(), indice, value);
+      printf("ciao, sono USER\n");
+		exit(EXIT_SUCCESS);
+    default:
+    printf("I'm a mother\n");
+    }
+  }
 
 	int shm_ID = shmget(4321, SO_REGISTRY_SIZE * SO_BLOCK_SIZE * sizeof(transazione), 0600 | IPC_CREAT);
 	printf("id[%d]\n", shm_ID);
