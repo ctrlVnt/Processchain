8c37baf5f97f8027121937226cdaa501a97dee9a
Cosmok21 <lavriccosmin@gmail.com>
2021-12-27 11:24:45 +0100

    piccola correzione

--- dev/MasterBtest.c
+++ dev/MasterBtest.c
@@ -166,6 +166,9 @@ void attesaNonAttiva(long, long);
 void signalHandler(int);
 /*calcolo del bilancio*/
 int calcoloBilancio();
+/*funzione che sceglie casualmente un nodo a cui inviare*/
+int scelgoNodo();
+
 
 int main()
 {
