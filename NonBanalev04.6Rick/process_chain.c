525af03fa6339135a9fe64ea012f812cc87f53d2
liveprog <giordanobruno227@gmail.com>
2022-01-03 02:29:36 +0100

    provato a fare makefile funzionale, fallito, provato algoritmo, fallito

---
+++ NonBanalev04.6Rick/process_chain.c
@@ -0,0 +1,8 @@
+#include "process_chain.h"
+
+/***********************/
+/***COMPILED***TIME*****/
+/***********************/
+int SO_BLOCK_SIZE = 10;
+int SO_TP_SIZE = 20;
+/***********************/
