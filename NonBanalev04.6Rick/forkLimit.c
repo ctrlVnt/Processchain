ab50f638d903016739b62a90569505f5b573f6b5
liveprog <giordanobruno227@gmail.com>
2022-01-04 11:24:58 +0100

    test limit fork

--- NonBanalev04.6Rick/forkLimit.c
+++ NonBanalev04.6Rick/forkLimit.c
@@ -1,22 +1,26 @@
 #include <errno.h>
+#include <stdlib.h>
 
 int main(){
     int i = 0;
     int child;
     while (child != EAGAIN)
     {
+        i++;
         child = fork();
 
         switch (child)
         {
         case -1:
-            perror("cazzo\n");
+            printf("cazzo %d\n", i);
+            sleep(1000);
             break;
         case 0:
-            printf("sono nato\n");
+            printf("sono nato %d\n", i);
+            sleep(1000);
+            exit(EXIT_SUCCESS);
             break;
         default:
-        i++;
             break;
         }
     }
